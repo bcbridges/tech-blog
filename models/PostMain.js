ce0272d5776c9e24f627ff1e500d13f704b2f1aa
bcbridges <bcb35b@gmail.com>
2021-09-09 06:28:59 -0600

    Added and populated post_main model.

---
+++ models/PostMain.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = required("sequelize");
+
+const sequelize = require("../config/connection.js");
+const User = require("./User");
+
+class PostMain extends Model {}
+
+PostMain.init(
+  {
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    creator_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "user_id",
+      },
+    },
+    post_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: false,
+    modelName: "post_main",
+  }
+);
+
+module.exports = PostMain;
