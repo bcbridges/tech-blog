c6eccd252ecdcf4f444cc7d396a8e3d9c23ffacf
bcbridges <bcb35b@gmail.com>
2021-09-09 08:43:20 -0600

    Added test seed data for post_comment.

---
+++ seeds/postcommentData.js
@@ -0,0 +1,19 @@
+const { PostComment } = require("../models");
+
+const postcommentdata = [
+  {
+    post_id: "2",
+    user_id: "1",
+    comment_body:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?",
+  },
+  {
+    post_id: "2",
+    user_id: "2",
+    comment_body: "Nullam non nisi est sit amet. :)",
+  },
+];
+
+const seedPostComment = () => PostComment.bulkCreate(postcommentdata);
+
+module.exports = seedPostComment;
