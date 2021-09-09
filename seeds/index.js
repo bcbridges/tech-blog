926f07b00bf9367c68cacca04c4ea8390f8a3e34
bcbridges <bcb35b@gmail.com>
2021-09-09 09:09:17 -0600

    Updated index.js to seed all tables will associated seed data.

---
+++ seeds/index.js
@@ -0,0 +1,21 @@
+const sequelize = require("../config/connection.js");
+const seedUser = require("./userData");
+const seedTagMain = require("./tagmainData");
+const seedPostMain = require("./postmainData");
+const seedPostComment = require("./postcommentData");
+const seedPostTag = require("./posttagData");
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  // Loops through to seed all data from concurrent js files
+  await seedUser();
+  await seedTagMain();
+  await seedPostMain();
+  await seedPostTag();
+  await seedPostComment();
+
+  process.exit(0);
+};
+
+seedAll();
