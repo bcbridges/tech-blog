08b81be929dd49d26b19cbde93152b02cbf24472
bcbridges <bcb35b@gmail.com>
2021-09-09 09:54:49 -0600

    Changed get / to test db seeded data.

--- controllers/login-routes.js
+++ controllers/login-routes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
+const { User } = require("../models");
 
 router.get("/", async (req, res) => {
-  res.render("login");
+  res.json(await User.findAll());
+  // res.render("login");
 });
 
 module.exports = router;
