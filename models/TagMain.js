a31a3bd51dcb3fa639531a78203056bd421415af
bcbridges <bcb35b@gmail.com>
2021-09-09 06:26:29 -0600

    Added module export to tag_main model.

--- models/TagMain.js
+++ models/TagMain.js
@@ -4,15 +4,26 @@ const sequelize = require("../config/connection.js");
 
 class TagMain extends Model {}
 
-TagMain.init({
-  tag_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+TagMain.init(
+  {
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_keyword: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  tag_keyword: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: false,
+    modelName: "tag_main",
+  }
+);
+
+module.exports = TagMain;
