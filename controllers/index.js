448637a35b5f5eb1f798bc0e6ab0358eca9834f6
bcbridges <bcb35b@gmail.com>
2021-09-09 11:13:40 -0600

    Updated seed routes with possible post.

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const loginRoutes = require("./login-routes");
+const seedRoutes = require("./seed-routes");
 
 router.use("/", loginRoutes);
+router.use("/seed", seedRoutes);
 
 module.exports = router;
