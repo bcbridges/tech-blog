1342293059fa7c4e43af81d94ffb6b0b827e6daf
bcbridges <bcb35b@gmail.com>
2021-09-13 14:53:35 -0600

    Added redirection to dashboard after user sign up.

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 
 const loginRoutes = require("./login-routes");
+const dashboardRoutes = require("./dashboard-routes");
 const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
+router.use("/dashboard", dashboardRoutes);
 router.use("/", loginRoutes);
 
 module.exports = router;
